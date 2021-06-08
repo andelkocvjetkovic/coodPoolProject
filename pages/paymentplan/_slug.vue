283f0cfc1879c1994764e3441579a7e4b6768c98
Andelko Cvjetkovic <cvjetkovic.andjelko@edu.fit.ba>
2021-06-08 15:55:07 +0200

    Updating to sass pages payment-plan

--- pages/paymentplan/_slug.vue
+++ pages/paymentplan/_slug.vue
@@ -1,38 +1,20 @@
 <template>
-  <section
-    class="text-blue-darkest mt-[38px] sm:max-w-[470px] sm:mx-auto lg:mt-0"
-  >
-    <section class="text-center">
-      <h1 class="text-2xl font-bold">We are sorry to see you go</h1>
-      <p class="text-base mt-2 sm:p-3">
+  <section class="confirm-cancel-page">
+    <section class="confirm-cancel-page__title">
+      <h1 class="confirm-cancel-page__title__header">
+        We are sorry to see you go
+      </h1>
+      <p class="confirm-cancel-page__title__text">
         Before you unsubscribe consider changing the payment plan. If you are
         uncertain which plan best suites your business do not hesitate to
         contact our support and we will do our best to assist you in choosing
         the right plan for you.
       </p>
     </section>
-    <section class="mt-[88px]">
-      <h2 class="text-lg text-center font-bold uppercase">
-        OUR JOURNEY SO FAR
-      </h2>
-      <div class="mt-4 flex flex-col gap-y-2 sm:flex-row sm:gap-x-2">
-        <p
-          v-for="(item, i) in $options.ourJourney"
-          :key="i"
-          class="
-            flex flex-col
-            justify-center
-            items-center
-            bg-blue
-            text-white
-            px-6
-            py-4
-            rounded
-            gap-y-4
-            sm:w-1/4
-            sm:flex-grow
-          "
-        >
+    <section class="confirm-cancel-page__cards">
+      <h2 class="confirm-cancel-page__cards__header">OUR JOURNEY SO FAR</h2>
+      <div class="confirm-cancel-page__cards__body">
+        <p v-for="(item, i) in $options.ourJourney" :key="i" class="blue-card">
           <span class="text-4xl font-bold">{{ item.description }}</span>
           <span class="text-base uppercase">{{ item.name }}</span>
         </p>
@@ -78,4 +60,68 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.confirm-cancel-page {
+  margin-top: 2.375rem;
+
+  @include respond-to("sm") {
+    max-width: 29.4rem;
+    margin: 0 auto;
+  }
+  @include respond-to("lg") {
+    margin-top: 0;
+  }
+
+  &__title {
+    text-align: center;
+    &__header {
+      @extend %text-2xl;
+      font-weight: bold;
+    }
+    &__text {
+      @extend %text-base;
+      margin-top: 0.5rem;
+
+      @include respond-to("sm") {
+        padding: 0.75rem;
+      }
+    }
+  }
+  &__cards {
+    margin-top: 5.5rem;
+    &__header {
+      @extend %text-lg;
+      font-weight: bold;
+      text-transform: uppercase;
+      text-align: center;
+    }
+    &__body {
+      margin-top: 0.5rem;
+      display: flex;
+      flex-direction: column;
+      column-gap: 0.5rem;
+
+      @include respond-to("sm") {
+        flex-direction: row;
+        row-gap: 0.5rem;
+      }
+    }
+  }
+}
+
+.blue-card {
+  background-color: $blue;
+  color: $white;
+  display: flex;
+  row-gap: 1rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 1.5rem;
+  border-radius: 0.25rem;
+
+  @include respond-to("sm") {
+    flex: 1 1 33.33%;
+  }
+}
+</style>
--- pages/paymentplan/index.vue
+++ pages/paymentplan/index.vue
@@ -1,37 +1,14 @@
 <template>
-  <section class="relative">
-    <div
-      class="
-        flex flex-col
-        gap-y-[14px]
-        sm:flex-row
-        sm:gap-x-[14px]
-        lg:max-w-[589px]
-        lg:mx-auto
-      "
-    >
+  <section class="payment-plan-page">
+    <div class="payment-plan-page__wrapper">
       <FeatureItem
         v-for="feature in $options.featuresList"
         :key="feature.header"
         :feature-item="feature"
-        class="xs:w-1/2 xs:mx-auto sm:mx-0 sm:w-1/6 flex-grow"
+        class="payment-plan-page__wrapper__item"
       />
     </div>
-    <AppButtonTeal
-      class="
-        mt-8
-        relative
-        left-full
-        transform
-        -translate-x-full
-        lg:absolute
-        lg:mt-0
-        lg:left-auto
-        lg:translate-x-0
-        lg:right-0
-        lg:bottom-0
-      "
-    />
+    <AppButtonTeal class="payment-plan-page__button" />
   </section>
 </template>
 
@@ -42,4 +19,76 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.payment-plan-page {
+  margin-top: 1rem;
+  @include respond-to("lg") {
+    margin: 0;
+  }
+  &__wrapper {
+    /*
+          flex flex-col
+        gap-y-[14px]
+        sm:flex-row
+        sm:gap-x-[14px]
+        lg:max-w-[589px]
+        lg:mx-auto */
+
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.875rem;
+    align-items: center;
+    @include respond-to("sm") {
+      align-items: stretch;
+      flex-direction: row;
+      column-gap: 0.875rem;
+    }
+    @include respond-to("lg") {
+      max-width: 36.8rem;
+      margin: 0 auto;
+    }
+
+    &__item {
+      width: 70%;
+
+      @include respond-to("xs") {
+        flex-basis: 50%;
+      }
+      @include respond-to("sm") {
+        flex: 1 1 33%;
+        margin: 0;
+      }
+    }
+  }
+  &__button {
+    /*           mt-8
+        relative
+        left-full
+        transform
+        -translate-x-full
+        lg:absolute
+        lg:mt-0
+        lg:left-auto
+        lg:translate-x-0
+        lg:right-0
+        lg:bottom-0
+     */
+    margin-top: 2rem;
+    position: relative;
+    left: 50%;
+
+    transform: translateX(-50%);
+    @include respond-to("sm") {
+      left: 100%;
+      transform: translateX(-100%);
+    }
+    @include respond-to("lg") {
+      transform: translateX(-0%);
+      left: unset;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+    }
+  }
+}
+</style>
